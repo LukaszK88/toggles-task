21bf980d50abaf2d1b5b040fe00438d9f6076255
lukas <kowal.lukas88@gmail>
2018-06-17 14:19:53 +0100

    Removed comments

--- webpack.config.js
+++ webpack.config.js
@@ -17,24 +17,16 @@ const config = {
         port: 8090,
         historyApiFallback: true
     },
-
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
-
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
-
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
           {
-            test: /\.s?css$/,                    // made scss
+            test: /\.s?css$/,
             use: ExtractTextPlugin.extract({
               fallback: 'style-loader',
               use: ['css-loader?modules&importLoaders=1&localIdentName=[local]', 'sass-loader']
